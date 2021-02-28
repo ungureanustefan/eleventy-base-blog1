523cd69f3710c0264916f5523a211b34f74e55df
Stefan <ungureanustefan680@gmail.com>
2021-02-28 20:10:36 +0000

    CMS bug fixed

--- .eleventy.js
+++ .eleventy.js
@@ -14,6 +14,8 @@ module.exports = function(eleventyConfig) {
   eleventyConfig.setDataDeepMerge(true);
 
   eleventyConfig.addLayoutAlias("post", "layouts/post.njk");
+  eleventyConfig.addLayoutAlias("blog", "layouts/post.njk");
+
 
   eleventyConfig.addFilter("readableDate", dateObj => {
     return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat("dd LLL yyyy");
